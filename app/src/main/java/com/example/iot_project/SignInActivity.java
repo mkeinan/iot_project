3f6f6d75b1d1beeecea7b72d5c202e0f9b90a12f
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2020-04-16 01:13:35 +0300

    Completed steps of section 04_04 from course.
    However, a deprecated class GoogleApiClient is used in the video, we
    need to swap it later with GoogleApi which is the new method to handle
    authentication.
    
    see:
    https://android-developers.googleblog.com/2017/11/moving-past-googleapiclient_21.html
    for more info

--- app/src/main/java/com/example/iot_project/SignInActivity.java
+++ app/src/main/java/com/example/iot_project/SignInActivity.java
@@ -1,23 +1,40 @@
 package com.example.iot_project;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends Activity implements View.OnClickListener {
+
+    private SignInButton mSignInButton;
+    private GoogleApiClient mGoogleApiclient;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(this);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .requestProfile()
+                .build();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,4 +46,13 @@ public class SignInActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
