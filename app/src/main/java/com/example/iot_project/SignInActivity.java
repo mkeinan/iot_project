5159cf6d6a462b79ff6bab9565298b0872a02ba4
Maayan Keinan <44086866+mkeinan@users.noreply.github.com>
2020-04-16 01:25:36 +0300

    Completed steps of section 04_05

--- app/src/main/java/com/example/iot_project/SignInActivity.java
+++ app/src/main/java/com/example/iot_project/SignInActivity.java
@@ -1,22 +1,29 @@
 package com.example.iot_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Toast;
 
-public class SignInActivity extends Activity implements View.OnClickListener {
+public class SignInActivity extends AppCompatActivity
+        implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
+    private static final int SIGN_IN_REQUEST = 9001;
     private SignInButton mSignInButton;
     private GoogleApiClient mGoogleApiclient;
     private FirebaseAuth mFirebaseAuth;
@@ -34,6 +41,11 @@ public class SignInActivity extends Activity implements View.OnClickListener {
                 .requestProfile()
                 .build();
 
+        mGoogleApiclient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         mFirebaseAuth = FirebaseAuth.getInstance();
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -48,11 +60,23 @@ public class SignInActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        if (view.getId() == R.id.sign_in_button) {
+            signIn();
+        }
+    }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiclient);
+        startActivityForResult(signInIntent, SIGN_IN_REQUEST);
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Google Play Services Error!", Toast.LENGTH_LONG).show();
     }
 }
